b071e6acbf1a17793ac123a0836f26e4a53a052f
Martin Selin <martin1995@live.se>
2019-03-15 15:07:32 +0100

    added single output residual version

--- feature_by_feature.py
+++ feature_by_feature.py
@@ -232,14 +232,24 @@ def FBF_modular_deeptrack(
     translation_distance=5, # parameters for multiprocessing training
     SN_limits=[10,100], # parameters for multiprocessing training
     model_path="",
-    combination_layer_type='addition', # determines the type of layer used for combining the
+    combination_layer_type='sum', # determines the type of layer used for combining the
      # predictions. Addition and average only options at the moment
     ):
     """
-    Function implementing the more advanced modular FBF which has denser connctions.
-
+    Function implementing the more advanced modular FBF which has denser connctions and completely modular design.
     Inputs:
-
+        layer_size - size of first layer in model
+        train_generator - generator for images
+        output_shape - number of outputs from network, 3 for normal deeptrack
+        nbr_nodes_added - number of nodes to add at a time
+        sample_size - same as deeptrack
+        iteration_numbers - same as deeptrack
+        save_networks - if networks are to be saved automatically once training is finished
+        mp_training - use multiprocessing to speed up training. Not available for custom
+        image generators as supplied by train_generator, uses FBFs own image generator.
+        translation_distance - parameter for mp_training image generator, determines
+        the area the particle is allowed to appear in
+        SN_limits -
     Outputs:
 
     TODO - Implement weight decay in later layers and lowering learning rate
@@ -263,7 +273,7 @@ def FBF_modular_deeptrack(
             # i = 0 special case. No addition needed
             next_flattened = layers.Flatten()(next_node)
             next_output = layers.Dense(3)(next_flattened)
-            final_outptut = next_output
+            final_output = next_output
             output_list.append(next_output)
             flattened_list.append(next_flattened)
         else:
@@ -274,11 +284,11 @@ def FBF_modular_deeptrack(
             next_output = layers.Dense(3)(next_output)
             output_list.append(next_output)
             if combination_layer_type=='average':
-                final_outptut = layers.average(output_list)
+                final_output = layers.average(output_list)
             else:
-                final_outptut = layers.add(output_list)
+                final_output = layers.add(output_list)
             # Construct and compile network
-        network = models.Model(input_tensor,final_outptut)
+        network = models.Model(input_tensor,final_output)
         network.compile(optimizer='rmsprop', loss='mse', metrics=['mse', 'mae'])
         network.summary()
 
@@ -478,6 +488,174 @@ def fbf_modular_expand_layer(
             network.save(model_path+"L"+str(layer_no)+"_"+str((i+base_length+1)*nbr_nodes_added)+"F.h5") # fix layer_indexing
 
     return network,conv_list,output_list,flattened_list,input_tensor
+def single_output_modular_L1(
+    layer_size,
+    train_generator,
+    input_shape=(51,51,1),
+    output_shape=3,
+    nbr_nodes_added=1,
+    sample_sizes=(8, 32, 128, 512, 1024),
+    iteration_numbers=(401, 301, 201, 101, 51),
+    verbose=0.01,
+    save_networks=True,
+    mp_training = False, # use multiprocessing training
+    translation_distance=5, # parameters for multiprocessing training
+    SN_limits=[10,100], # parameters for multiprocessing training
+    model_path="",
+     ):
+    import deeptrack
+    import keras
+    from keras import  Input,models,layers
+    from keras.models import Model
+    #import deeptrackelli_mod_mproc as multiprocess_training # Needed for fast parallelized image generator
+
+    input_tensor = Input(input_shape)
+    conv_list = [] # List of convolutional neruons in L1, needed for subsequent layers
+    flattened_list = [] # List of flattened layers
+
+    # Loop thorugh the neoruns and add them one by one
+    for i in range(round(layer_size/nbr_nodes_added)):
+        next_node = layers.Conv2D(nbr_nodes_added,(3,3),activation='relu')(input_tensor)
+        next_node = layers.MaxPooling2D((2,2))(next_node)
+        if(i==0):
+            # i = 0 special case. No addition needed
+            next_flattened = layers.Flatten()(next_node)
+            flattened_list.append(next_flattened)
+            next_output = layers.Dense(3)(next_flattened)
+            final_output = next_output
+        else:
+            # Construct the next output node
+            next_flattened = layers.Flatten()(next_node)
+            flattened_list.append(next_flattened)
+            #Create temporary list with new output
+
+            next_output = layers.Concatenate(axis=-1)(flattened_list)
+            next_output = layers.Concatenate(axis=-1)([next_output,final_output])
+            final_output = layers.Dense(3)(next_output)
+
+        network = models.Model(input_tensor,final_output)
+        network.compile(optimizer='rmsprop', loss='mse', metrics=['mse', 'mae'])
+        network.summary()
+
+        # Train and freeze layers in network
+        if mp_training:
+            deeptrack.train_deep_learning_network_mp(
+                network,
+                sample_sizes = sample_sizes,
+                iteration_numbers = iteration_numbers,
+                verbose=verbose,
+                SN_limits=SN_limits,
+                translation_distance=translation_distance,
+                )
+        else:
+            deeptrack.train_deep_learning_network(
+                network,
+                train_generator,
+                sample_sizes = sample_sizes,
+                iteration_numbers = iteration_numbers,
+                verbose=verbose)
+
+        freeze_all_layers(network)
+        conv_list.append(next_node)
+        if(save_networks):
+            network.save(model_path+"L1_"+str((i+1)*nbr_nodes_added)+"F.h5")
+    return network,conv_list,flattened_list,input_tensor,final_output
+
+def modular_single_output_new_layer(
+    layer_size, # Total number of nodes in layer
+    train_generator,
+    conv_list,
+    final_output,
+    input_tensor,
+    layer_no=2,
+    nbr_nodes_added=1,
+    sample_sizes=(8, 32, 128, 512, 1024),
+    iteration_numbers=(401, 301, 201, 101, 51),
+    verbose=0.01,
+    mp_training = False, # use multiprocessing training?
+    translation_distance=5, # parameters for multiprocessing training
+    SN_limits=[10,100], # parameters for multiprocessing training
+    save_networks=True,
+    model_path="",
+    layer_type='conv'):
+    """
+    Function for adding a new layer in a modular fbf model, note that it adds a
+    convolutional layer with pooling.
+    """
+    import deeptrack
+    from keras import  models,layers
+    from keras.models import Model
+
+    new_layer_node_list = [] # Convolutions in the new layer
+    new_layer_flattened_list = [] # flattened output from new layer
+
+    # Create input tenosr for new node
+    if len(conv_list)>1:
+        new_layer_input = layers.Concatenate()(conv_list)
+    else:
+        new_layer_input = conv_list[0]
+
+    # If next layer is dense then we (probably) need to flatten previous input
+    if layer_type=='dense':
+        import keras.backend as K
+        # Check dimension of previous output to see if Flatten layer is neede.
+        prev_out_size = K.shape(new_layer_input).shape
+        if(prev_out_size[0]>2):
+            new_layer_input = layers.Flatten()(new_layer_input)
+
+    # Add all the new nodes and train network in between
+    for i in range(round(layer_size/nbr_nodes_added)):
+        if layer_type=='dense':
+            next_node = layers.Dense(nbr_nodes_added,activation='relu')(new_layer_input)
+            next_flattened = next_node
+        else:
+            next_node = layers.Conv2D(nbr_nodes_added,(3,3),activation='relu')(new_layer_input)
+            next_node = layers.MaxPooling2D((2,2))(next_node)
+            next_flattened = layers.Flatten()(next_node)
+        new_layer_flattened_list.append(next_flattened)
+        if(i==0):
+            # i = 0 special case. No concatenation need
+            next_output = layers.Concatenate(axis=-1)([next_flattened,final_output])
+            next_output = layers.Dense(3)(next_output) # Different for i==0
+
+        else:
+            #tmp_list = new_layer_flattened_list
+            next_output = layers.Concatenate(axis=-1)(new_layer_flattened_list)
+            next_output = layers.Concatenate(axis=-1)([next_output,final_output])
+            next_output = layers.Dense(3)(next_output)
+        final_output = next_output
+
+        # Construct and compile network
+        network = models.Model(input_tensor,final_output)
+        network.compile(optimizer='rmsprop', loss='mse', metrics=['mse', 'mae'])
+        network.summary()
+
+        # Train and freeze layers in network
+
+        if mp_training:
+            deeptrack.train_deep_learning_network_mp(
+                network,
+                sample_sizes = sample_sizes,
+                iteration_numbers = iteration_numbers,
+                verbose=verbose,
+                SN_limits=SN_limits,
+                translation_distance=translation_distance,
+                )
+        else:
+            deeptrack.train_deep_learning_network(
+                network,
+                train_generator,
+                sample_sizes = sample_sizes,
+                iteration_numbers = iteration_numbers,
+                verbose=verbose)
+
+        freeze_all_layers(network)
+        new_layer_node_list.append(next_node)
+        if(save_networks):
+            network.save(model_path+"L"+str(layer_no)+"_"+str((i+1)*nbr_nodes_added)+"F.h5") # L2 all the time not optimal
+
+    # IF dense statement needed
+    return network,new_layer_node_list,final_output,new_layer_flattened_list,input_tensor
 
 def get_modular_terms_outputs(input_tensor,output_node_list,images):
     """
